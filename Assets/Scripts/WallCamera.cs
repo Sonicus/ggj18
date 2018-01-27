72d74ed5cc6b339c4b8b895ca27691f8a3b29e97
Jere Nevalainen <jere.nevalainen@gmail.com>
2018-01-27 18:21:56 +0200

    Possible to set an initial angle for cameras

--- Assets/Scripts/WallCamera.cs
+++ Assets/Scripts/WallCamera.cs
@@ -6,10 +6,13 @@ public class WallCamera : MonoBehaviour, IEnemy {
 
     public float turnAngle = 45.0f;
     public float turnSpeed = 0.25f;
+    public float initialAngle = 0.0f;
     private float turnedAngle = 0.0f;
 
 	// Use this for initialization
 	void Start () {
+        this.transform.Rotate(new Vector3(0, initialAngle, 0));
+        turnedAngle = initialAngle;
 	}
 	
 	// Update is called once per frame
