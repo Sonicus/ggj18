ca3772824819fc8d21f434a2c1d7e89eaaae7675
Jere Nevalainen <jere.nevalainen@gmail.com>
2018-01-27 18:10:14 +0200

    Larger BlackOfDeath image

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -84,6 +84,10 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
         if (gameLost)
         {
             CheckForReset();
